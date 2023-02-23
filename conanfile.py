d939564fac9cb789a82c8703063cb29a0489669a
Harald Held <harald.held@gmail.com>
2023-02-23 14:48:53 +0100

    Fix recipe for newer versions of Conan

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
 
 
 class MlpackConan(ConanFile):
@@ -10,7 +11,6 @@ class MlpackConan(ConanFile):
     description = "mlpack is an intuitive, fast, and flexible header-only C++ machine learning library with bindings to other languages."
     topics = ("machine learning")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
 
     requires = ["armadillo/10.7.3", "ensmallen/2.19.0", "cereal/1.3.2"]
     options = {"shared": [True, False], "fPIC": [True, False]}
@@ -23,18 +23,28 @@ class MlpackConan(ConanFile):
     def source(self):
         self.run(f"git clone -b {self.version} https://github.com/mlpack/mlpack.git")
 
+    def generate(self):
+       tc = CMakeToolchain(self)
+       tc.preprocessor_definitions["ARMADILLO_INCLUDE_DIR"] = self.dependencies["armadillo"].cpp_info.includedirs
+       tc.preprocessor_definitions["ARMADILLO_LIBRARY"] = self.dependencies["armadillo"].cpp_info.libs
+       tc.preprocessor_definitions["ENSMALLEN_INCLUDE_DIR"] = self.dependencies["ensmallen"].cpp_info.includedirs
+       tc.preprocessor_definitions["CEREAL_INCLUDE_DIR"] = self.dependencies["cereal"].cpp_info.includedirs
+       tc.preprocessor_definitions["BUILD_CLI_EXECUTABLES"] = "OFF"
+       tc.generate()
+
+       deps = CMakeDeps(self)
+       deps.generate()
+
     def build(self):
         cmake = CMake(self)
 
-        cmake.definitions["ARMADILLO_INCLUDE_DIR"] = self.dependencies["armadillo"].cpp_info.includedirs[0]
-        cmake.definitions["ARMADILLO_LIBRARY"] = self.dependencies["armadillo"].cpp_info.libs
-        cmake.definitions["ENSMALLEN_INCLUDE_DIR"] = self.dependencies["ensmallen"].cpp_info.includedirs[0]
-        cmake.definitions["CEREAL_INCLUDE_DIR"] = self.dependencies["cereal"].cpp_info.includedirs[0]
-        cmake.definitions["BUILD_CLI_EXECUTABLES"] = "OFF"
-        
-        cmake.configure(source_folder="mlpack")
+        cmake.configure(build_script_folder="mlpack")
         cmake.build()
+        cmake.install()
 
     def package(self):
         self.copy("*.h", dst="include", src="mlpack/src")
         self.copy("*.hpp", dst="include", src="mlpack/src")
+
+    def layout(self):
+            cmake_layout(self)
